3801c6f2dfd7737232368aec9dcca743380deb77
Trim21 <trim21.me@gmail.com>
2025-01-03 21:13:41 +0800

    tests: rename

---
+++ tests/e2e/04_getaddrinfo_default.py
@@ -0,0 +1,33 @@
+import asyncio
+import asyncio.futures
+import socket
+import threading
+
+from asioloop import AsioEventLoopPolicy
+
+asyncio.set_event_loop_policy(AsioEventLoopPolicy())
+
+loop = asyncio.SelectorEventLoop()
+
+
+def work():
+    print("run work in thread", threading.current_thread().native_id)
+    print("now try to resolve")
+    try:
+        result = socket.getaddrinfo("www.baidu.com", 443)
+    except Exception as e:
+        print(e)
+        raise
+    print("run work in thread", threading.current_thread().native_id)
+    print("resolve done")
+    return result
+
+
+async def main() -> None:
+    result = await loop.run_in_executor(None, work)
+
+    print("async main done", result)
+
+
+loop.run_until_complete(main())
+print("done")
